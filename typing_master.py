ee2915790278349a0e3dc07ec59958bb3021d86d
Kunal <kunaltyagi760@gmail.com>
2024-01-14 19:04:10 +0530

    Added user input code

---
+++ typing_master.py
@@ -0,0 +1,16 @@
+def main():
+    print("Welcome to Terminal Typing Master!")
+
+    # Get username
+    username = input("Enter your username: ")
+
+    while True:
+        # Display menu options
+        print("\nMenu:")
+        print("1. Start Typing Test")
+        print("2. Show Leaderboard")
+        print("3. Exit")
+
+        choice = input("Enter your choice (1, 2, or 3): ")
+
+main()
